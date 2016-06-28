018f9af32d3cb35613717e74843c4eb6f1426b0a
Ryan <hhrl2@cam.ac.uk>
2016-06-28 10:34:24 +0100

    Fully added annotations to the whole test suite.

--- src/test/java/org/isaacphysics/labs/chemistry/checker/TestParser.java
+++ src/test/java/org/isaacphysics/labs/chemistry/checker/TestParser.java
@@ -332,7 +332,12 @@ public class TestParser
         assertFalse("Two expression statements containing errors recognised as equal!",
                     exprStatement1.equals(exprStatement2));
     }
-    
+
+    /**
+     * Simple test. Parses two expressions with differing arrow symbols. Checks if system manages to differentiate
+     * between the two expressions.
+     * @throws Exception
+     */
     @Test
     public void testArrowDifference() throws Exception
     {
@@ -340,17 +345,25 @@ public class TestParser
         String fake = "2CO -> CO2 + C";
         ArrayList<Statement> statements = stringParser(boudouard_reaction + ";" + fake);
 
+        // Assumes only two statements are parsed.
         assertTrue("Expected two statements, got " + statements.size(), statements.size() == 2);
 
         Statement first = statements.get(0);
         Statement second = statements.get(1);
 
+        // Assume both to be chemical equations.
         assertTrue("2CO <--> CO2 + C should be of type EquationStatement.", first instanceof EquationStatement);
         assertTrue("2CO -> CO2 + C should be of type EquationStatement.", second instanceof EquationStatement);
 
+        // Assume both to be different in semantic meaning.
         assertFalse(boudouard_reaction + ";" + fake + " are considered the same.", first.equals(second));
     }
 
+    /**
+     * Simple test. Both statements mix up chemical terms with nuclear ones. The parser should be able to spot error
+     * in parsing stage.
+     * @throws Exception
+     */
     @Test
     public void testInvalidExpressions() throws Exception
     {
@@ -359,49 +372,72 @@ public class TestParser
 
         ArrayList<Statement> statements = stringParser(invalidExpression + ";" + invalidEquation);
 
+        // Assumes only two statements are parsed.
         assertTrue("Expected two statements, got " + statements.size(), statements.size() == 2);
 
         Statement first = statements.get(0);
         Statement second = statements.get(1);
 
+        // Assumes both expressions contain error terms.
         assertTrue(invalidExpression + " not recognised as containing an error!", first.containsError());
         assertTrue(invalidEquation + " not recognised as containing an error!", second.containsError());
     }
 
+    /**
+     * Easy test. Invalid tokens such as state symbols, coefficients and double arrow are placed in nuclear expressions.
+     * The parser should be able to detect such errors.
+     * @throws Exception
+     */
     @Test
     public void testInvalidNuclear() throws Exception
     {
+        // State symbols should not be assigned to isotopes.
         String stateSymbol = "^{226}_{88}Ra (s) -> /alpha_particle + ^{222}_{86}Rn (aq)";
+
+        // Coefficients should not be assigned to isotopes.
         String coefficient = "^{14}_{6}C -> 20^{14}_{7}N + /electron";
+
+        // No equilibrium stuff for nuclear reactions.
         String equilibrium = "^{131}_{53}I <--> ^{131}_{54}Xe + /electron";
 
         ArrayList<Statement> statements = stringParser(stateSymbol + ";" +
                 coefficient + ";" + equilibrium);
 
+        // Assumes only three statements are parsed.
         assertTrue("Expected three statements, got " + statements.size(), statements.size() == 3);
 
         Statement first = statements.get(0);
         Statement second = statements.get(1);
         Statement third = statements.get(2);
 
+        // Assumes all three expressions have errors.
         assertTrue(stateSymbol + " not recognised as containing an error!", first.containsError());
         assertTrue(coefficient + " not recognised as containing an error!", second.containsError());
         assertTrue(equilibrium + " not recognised as containing an error!", third.containsError());
     }
 
+    /**
+     * Test on validity of atomic number. For atomic number to be valid, atomic number should match the element symbol
+     * for all isotopes. The mass number should not be smaller than the atomic number as well.
+     * @throws Exception
+     */
     @Test
     public void checkAtomicNumber() throws Exception
     {
         String alphaDecay = "^{243}_{95}Am + ^{48}_{20}Ca -> ^{288}_{115}Uup + 3/neutrino";
+
+        // Uut does not have an atomic number of 112.
         String invalidAtomicNumber = "^{283}_{112}Uut + 3/alpha_particle ";
 
         ArrayList<Statement> statements = stringParser(alphaDecay + ";" + invalidAtomicNumber);
 
+        // Assumes only two statements are parsed.
         assertTrue("Expected two statements, got " + statements.size(), statements.size() == 2);
 
         Statement first = statements.get(0);
         Statement second = statements.get(1);
 
+        // Assumes both expressions to be nuclear equations.
         assertTrue(first + " should be of type NuclearEquationStatement.",
                 first instanceof NuclearEquationStatement);
         assertTrue(second + " should be of type NuclearExpressionStatement.",
@@ -410,23 +446,40 @@ public class TestParser
         NuclearEquationStatement eq = (NuclearEquationStatement) first;
         NuclearExpressionStatement ex = (NuclearExpressionStatement) second;
 
+        // Assumes first statement has valid atomic numbers.
         assertTrue(alphaDecay + " should have correct atomic numbers.", eq.isValid());
+
+        // Assumes program is able to detect invalid atomic number in expression.
         assertFalse(invalidAtomicNumber + " should have incorrect atomic numbers.", ex.isValid());
     }
 
+    /**
+     * Checks if nuclear equations are balanced, and have valid atomic numbers. By balanced we mean that:
+     * <p>
+     *     <ul>
+     *         <li>Mass numbers are the same on both sides.</li>
+     *         <li>Atomic numbers are equal on both sides.</li>
+     *     </ul>
+     * </p>
+     * @throws Exception
+     */
     @Test
     public void checkNuclearBalance() throws Exception
     {
         String c14_decay = "^{14}_{6}C -> ^{14}_{7}N + /electron";
+
+        // Unbalanced mass number.
         String bad_decay = "^{12}_{6}C -> ^{14}_{7}N + /electron";
 
         ArrayList<Statement> statements = stringParser(c14_decay + ";" + bad_decay);
 
+        // Assumes only two statements are parsed.
         assertTrue("Expected two statements, got " + statements.size(), statements.size() == 2);
 
         Statement first = statements.get(0);
         Statement second = statements.get(1);
 
+        // Assumes both statements to be nuclear equations.
         assertTrue(first + " should be of type NuclearEquationStatement.",
                 first instanceof NuclearEquationStatement);
         assertTrue(second + " should be of type NuclearEquationStatement.",
@@ -435,22 +488,32 @@ public class TestParser
         NuclearEquationStatement eq = (NuclearEquationStatement) first;
         NuclearEquationStatement eq2 = (NuclearEquationStatement) second;
 
+        // Assumes first statement to be balanced and valid.
         assertTrue(c14_decay + " should have balanced atomic and mass numbers.", eq.isBalanced());
         assertTrue(c14_decay + " should have valid atomic numbers.", eq.isValid());
+
+        // Assumes second statement to be unbalanced yet valid.
         assertFalse(bad_decay + "should not have balanced atomic and mass numbers.", eq2.isBalanced());
         assertTrue(bad_decay + " should have valid atomic numbers.", eq2.isValid());
 
+        // Assumes both statements contain no error terms.
         assertFalse(c14_decay + " unexpectedly recognised as containing an error!", eq.containsError());
         assertFalse(bad_decay + " unexpectedly recognised as containing an error!", eq2.containsError());
     }
 
+    /**
+     * Tests equivalence of nuclear expressions.
+     * @throws Exception
+     */
     @Test
     public void testNuclearExpressionEquivalence() throws Exception
     {
+        // Nuclear expressions: Set 1
         String eStream = "/electron + /electron + /neutrino";
         String eStream2 = "2/electron + /neutrino";
         String eStream3 = "/electron + /neutrino + /neutrino";
 
+        // Nuclear expression: Set 2
         String alphaDecay = "^{243}_{95}Am + ^{48}_{20}Ca -> ^{288}_{115}Uup + 3/neutrino";
         String alphaDecay2 = "^{48}_{20}Ca + ^{243}_{95}Am -> 3/neutrino + ^{288}_{115}Uup";
         String alphaDecay3 = "^{48}_{20}Ca + ^{243}_{95}Am -> ^{288}_{115}Uup";
@@ -463,6 +526,7 @@ public class TestParser
                                                         alphaDecay2 + ";" +
                                                         alphaDecay3);
 
+        // Assumes only six statements are parsed.
         assertTrue("Expected six statements, got " + statements.size(), statements.size() == 6);
 
         Statement first  = statements.get(0);
@@ -472,12 +536,24 @@ public class TestParser
         Statement fifth  = statements.get(4);
         Statement sixth  = statements.get(5);
 
+        // Assumes that all statements in set 1 are pairwise non-equivalent.
         assertFalse("Expected eStream != eStream2.", first.equals(second));
         assertFalse("Expected eStream != eStream3.", first.equals(third));
+
+        // Assumes first 2 statements in set 2 to be equivalent.
+        // Also assumes second expression non-equivalent to third one.
         assertTrue("Expected alphaDecay == alphaDecay2.", fourth.equals(fifth));
         assertFalse("Expected alphaDecay2 != alphaDecay3.", fifth.equals(sixth));
     }
 
+    /**
+     * Testing out the weak equivalence method.
+     * Two statements are weakly equivalent if they are of the same type, and contains same chemical formulae,
+     * disregarding coefficients and state symbols.
+     *
+     * For instance NaOH(aq) is weakly equivalent to 100NaOH.
+     * @throws Exception
+     */
     @Test
     public void testWeakEquivalence() throws Exception
     {
@@ -491,6 +567,7 @@ public class TestParser
                                                         much_wrong + ";" +
                                                         error_term);
 
+        // Assumes only four statements are parsed.
         assertTrue("Expected 4 statements, got " + statements.size(), statements.size() == 4);
 
         Statement first  = statements.get(0);
@@ -498,45 +575,70 @@ public class TestParser
         Statement third  = statements.get(2);
         Statement fourth = statements.get(3);
 
+        // Assumes that all four statements are chemical equations.
         assertTrue("All four statements should be EquationStatement.", first instanceof EquationStatement &&
                                                                         second instanceof EquationStatement &&
                                                                         third instanceof EquationStatement &&
                                                                         fourth instanceof EquationStatement);
 
+        // Assumes that the first four terms are mutually equivalent.
         assertTrue("Expected acid_base ~= acid_base.", first.weaklyEquivalent(first));
         assertTrue("Expected acid_base ~= missing_state.", first.weaklyEquivalent(second));
         assertTrue("Expected acid_base ~= much_wrong.", first.weaklyEquivalent(third));
         assertTrue("Expected missing_state ~= much_wrong.", second.weaklyEquivalent(third));
+
+        // Assumes last term is not equivalent to any other terms.
         assertFalse("Expected acid_base !~= error_term.", first.weaklyEquivalent(fourth));
 
         EquationStatement one = (EquationStatement) first;
         EquationStatement two = (EquationStatement) second;
         EquationStatement three = (EquationStatement) third;
 
+        // Assumes first statement agrees with second statement on coefficient, but not on state.
         assertTrue("Expected acid_base == missing_state on coefficient.", one.sameCoefficients(two));
         assertFalse("Expected acid_base != missing_state on state.", one.sameStateSymbols(two));
+
+        // Assumes first statement disagrees with fourth statement on both coefficient and state.
         assertFalse("Expected acid_base != much_wrong on coefficient.", one.sameCoefficients(three));
         assertFalse("Expected acid_base != much_wrong on state.", one.sameStateSymbols(three));
-
     }
 
+    /**
+     * Testing out new class Hydrate. It is an extension of uncharged compound, and has form X.n H2O.
+     * Though it is also common for people to write X(H2O)n instead, this form clearly shows the water in crystallization.
+     * The software should be able to count atoms in hydrate correctly.
+     *
+     * @throws Exception
+     */
     @Test
     public void testHydrates() throws Exception
     {
         String hydrate = "CaCl2.2H2O -> CaCl2 + 2H2O";
 
         ArrayList<Statement> statements = stringParser(hydrate);
+
+        // Assumes that only one statement is parsed.
         assertTrue("Expected 1 statement, got " + statements.size(), statements.size() == 1);
 
         Statement first = statements.get(0);
 
+        // Assumes that statement is chemical equation.
         assertTrue("Expected hydrate to have type EquationStatement.", first instanceof EquationStatement);
 
         EquationStatement equation = (EquationStatement) first;
 
+        // Expect equation to be balanced.
         assertTrue("Expected hydrate to be balanced.", equation.isBalanced());
     }
 
+    /**
+     * Test out the special syntax for isotopes.
+     * Originally isotopes could only be written in form ^{A}_{B}X, but now _{B}^{A}X is also allowed.
+     * <p>
+     * This test is used to make sure that no error is introduced in modification.
+     * For instance the mass numbers and atomic numbers may be messed up.
+     * @throws Exception
+     */
     @Test
     public void testIsotopes() throws Exception
     {
@@ -549,17 +651,21 @@ public class TestParser
         Statement first  = statements.get(0);
         Statement second = statements.get(1);
 
+        // Assumes both statements to be nuclear equations.
         assertTrue("Expected alphaDecay to have type NuclearEquationStatement.",
                                                         first instanceof NuclearEquationStatement);
         assertTrue("Expected sameDecay to have type NuclearEquationStatement.",
                                                         second instanceof NuclearEquationStatement);
 
+        // Assumes first statement to be balanced, and valid.
         assertTrue("Expected alphaDecay to be balanced.", ((NuclearEquationStatement) first).isBalanced());
         assertTrue("Expected alphaDecay to have valid atomic numbers.", ((NuclearEquationStatement) first).isValid());
 
+        // Assumes second statement to be balanced and valid as well.
         assertTrue("Expected sameDecay to be balanced.", ((NuclearEquationStatement) second).isBalanced());
         assertTrue("Expected sameDecay to have valid atomic numbers.", ((NuclearEquationStatement) second).isValid());
 
+        // Expect both statements to be equivalent.
         assertTrue("Expected alphaDecay == sameDecay.", first.equals(second));
     }
 
